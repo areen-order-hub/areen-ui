920414e0fb34ceffcf56efddcc1a61c5198600ce
bmonish <monishmonib@gmail.com>
2022-12-25 23:04:31 +0530

    Modified Deploy Script

--- deploy-aws.sh
+++ deploy-aws.sh
@@ -4,11 +4,14 @@ pm2 stop 1
 echo "Using NVM 12.18.3"
 nvm use 12.18.3
 
-echo "Changing Directory to API"
+echo "Changing Directory to UI"
 cd areen-ui
 
 echo "Pulling from git"
 git pull
+
+echo "Installing dependencies"
+npm install
 
 echo "Starting server"
 npm run start:prod
