8452e3e05642b58a1f592e37b9a2adc45e3364d1
bmonish <monishmonib@gmail.com>
2023-06-19 13:04:35 +0530

    AOH-190 - Added Orders Export with Date Range

--- app/containers/Orders/constants.js
+++ app/containers/Orders/constants.js
@@ -5,6 +5,8 @@
  */
 
 export const SET_ORDER_LIST = "app/Orders/SET_ORDER_LIST";
+export const SET_IS_EXPORT_LOADING = "app/Orders/SET_IS_EXPORT_LOADING";
+export const SET_ORDERS_FOR_EXPORT = "app/Orders/SET_ORDERS_FOR_EXPORT";
 export const SET_STORE_LIST = "app/Orders/SET_STORE_LIST";
 export const SET_IS_SHIPMENT_GENERATING =
   "app/Orders/SET_IS_SHIPMENT_GENERATING";
